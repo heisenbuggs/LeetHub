0a644c159990fd4c70bdefd38fc58e68b9d932b5
Prasuk Jain <60914425+heisenbuggs@users.noreply.github.com>
2024-07-27 22:27:45 +0530

    Time: 1559 ms (5.01%) | Memory: 8.8 MB (25.76%) - LeetSync

---
+++ 1203-print-in-order/print-in-order.cpp
@@ -0,0 +1,28 @@
+class Foo {
+public:
+    std::atomic<int> a = 0;
+    Foo() {
+        
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        a++;
+    }
+
+    void second(function<void()> printSecond) {
+        while(a!=1);
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        a++;
+    }
+
+    void third(function<void()> printThird) {
+        while(a!=2);
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+        a++;
+    }
+};
