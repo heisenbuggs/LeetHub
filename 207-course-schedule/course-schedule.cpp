f831d61ae29143be689a576f83641d062fa447e5
Prasuk Jain <60914425+heisenbuggs@users.noreply.github.com>
2024-08-03 11:07:04 +0530

    Time: 13 ms (78.82%) | Memory: 17.2 MB (64.84%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> ind(numCourses, 0), ans;
+        vector<vector<int>> list(numCourses);
+        queue<int> q;
+
+        for(int i=0; i<prerequisites.size(); i++) {
+            list[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            ind[prerequisites[i][0]]++;
+        }
+
+        for(int i=0; i<numCourses; i++) {
+            if(ind[i]==0) q.push(i);
+        }
+
+        while(!q.empty()) {
+            int x = q.front();
+            q.pop();
+            ans.push_back(x);
+
+            for(auto it: list[x]) {
+                ind[it]--;
+                if(ind[it]==0) q.push(it);
+            }
+        }
+
+        return ans.size() == numCourses ? true : false;
+    }
+};
